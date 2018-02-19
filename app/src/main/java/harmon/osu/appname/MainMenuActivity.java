1864d0623be0132c99c460a76bd17b7eac9e2d15
justinheer <justinheer1@gmail.com>
2018-02-19 17:51:51 -0500

    added mainmenu

---
+++ app/src/main/java/harmon/osu/appname/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package harmon.osu.appname;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Justin on 2/19/2018.
+ */
+
+public class MainMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
