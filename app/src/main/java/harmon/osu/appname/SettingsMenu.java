7cc6e24ccb03bea3d1b52afb1b8235c530a6883e
Theharm0n95 <31480399+Theharm0n95@users.noreply.github.com>
2018-02-21 10:01:49 -0500

    Settings layout created

--- app/src/main/java/harmon/osu/appname/SettingsMenu.java
+++ app/src/main/java/harmon/osu/appname/SettingsMenu.java
@@ -3,11 +3,11 @@ package harmon.osu.appname;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoadingScreenActivity extends AppCompatActivity {
+public class SettingsMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loading_screen);
+        setContentView(R.layout.settings_menu);
     }
 }
