7a0eb7410661e5c962bd5df9dc1737c024162fe5
justinheer <justinheer1@gmail.com>
2018-03-02 23:39:19 -0500

    Added the ability to take pictures when using real phone.

--- app/src/main/java/harmon/osu/appname/MainMenuActivity.java
+++ app/src/main/java/harmon/osu/appname/MainMenuActivity.java
@@ -1,11 +1,18 @@
 package harmon.osu.appname;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
+import java.util.Calendar;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Justin on 2/19/2018.
@@ -56,5 +63,28 @@ public class MainMenuActivity extends AppCompatActivity {
     public void toSettings(View v){
         Intent i = new Intent(this,SettingsMenuActivity.class);
         startActivity(i);
+    }
+
+    public void takePicture(View v) {
+        // Create photo directory
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/" + getString(R.string.app_name) + "/pictures/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+        // Make new file
+        String file = dir + Calendar.getInstance().getTime().toString() +".jpg";
+        File newfile = new File(file);
+        try {
+            newfile.createNewFile();
+        }
+        catch (IOException e)
+        {
+        }
+        // Take photo
+        Uri outputFileUri = Uri.fromFile(newfile);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivity(cameraIntent);
     }
 }
